9306c80d8af65989643d834f606b603ca4517d6e
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2025-04-04 22:58:18 +0200

    Update send-sol.ts

--- send-sol.ts
+++ send-sol.ts
@@ -18,3 +18,22 @@ if (!privateKey) {
 const asArray = Uint8Array.from(JSON.parse(privateKey));
 const sender = Keypair.fromSecretKey(asArray);
 connection = new Connection(clusterApiUrl("devnet"))
+console.log(`🔑 Our public key is: ${sender.publicKey.toBase58()}`);
+
+const recipient = new PublicKey("4iE8y7RuUEiu231FU38ur48Ehi343uR0324rjJ"); // edit
+console.log(`💸 Attempting to send 0.01 SOL to ${recipient.toBase58()}...`);
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: sender.publicKey,
+  toPubkey: recipient,
+  lamports: 0.01 * LAMPORTS_PER_SOL,
+});
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  sender,
+]);
+
+console.log(`✅ Transaction confirmed, signature: ${signature}!`);
